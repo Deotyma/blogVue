a174531bb15ade2c7beff51581f761cd1febbc4e
Ewa <kalistaewa@gmail.com>
2021-11-04 01:40:24 +0100

    adds MDB Vue UI

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,67 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <header>
+    <!-- Navbar -->
+      <MDBNavbar expand="lg" light bg="white" container>
+        <MDBNavbarToggler target="#navbarExample01"></MDBNavbarToggler>
+        <MDBNavbarNav collapse="navbarExample01" class="mb-2 mb-lg-0">
+          <MDBNavbarItem to="#" active>
+            Home
+          </MDBNavbarItem>
+          <MDBNavbarItem to="#">
+            Features
+          </MDBNavbarItem>
+          <MDBNavbarItem to="#">
+            Pricing
+          </MDBNavbarItem>
+          <MDBNavbarItem to="#">
+            About
+          </MDBNavbarItem>
+        </MDBNavbarNav>
+      </MDBNavbar>
+    <!-- Navbar -->
+
+    <!-- Background image -->
+    <div
+      class="p-5 text-center bg-image"
+      style="
+        background-image: url('https://mdbcdn.b-cdn.net/img/new/slides/041.jpg');
+        height: 400px;
+      "
+    >
+      <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+        <div class="d-flex justify-content-center align-items-center h-100">
+          <div class="text-white">
+            <h1 class="mb-3">Heading</h1>
+            <h4 class="mb-3">Subheading</h4>
+            <MDBBtn tag="a" lg outline="light"
+              href="#!"
+              role="button"
+              >Call to action
+            </MDBBtn>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Background image -->
+  </header>
 </template>
 
+<script>
+  import { MDBBtn, MDBNavbar, MDBNavbarToggler, MDBNavbarNav, MDBNavbarItem } from 'mdb-vue-ui-kit';
+
+  export default {
+    components: {
+      MDBBtn,
+      MDBNavbar,
+      MDBNavbarToggler,
+      MDBNavbarNav,
+      MDBNavbarItem
+    },
+  };
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+ #app {
+  font-family: Roboto, Helvetica, Arial, sans-serif;
 }
 </style>
